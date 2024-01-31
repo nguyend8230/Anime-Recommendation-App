48a12650872c34c8fba3a3961a857aa7da7b2b72
nguyend8230 <nguyend8230@gmail.com>
2024-01-31 16:09:28 +0700

    implemented a redis cache in the backend

--- backend/controllers/anime_controller.js
+++ backend/controllers/anime_controller.js
@@ -3,40 +3,47 @@ const redis = require("redis");
 const Anime = require("../models/anime_model");
 const Recommendation = require("../models/recommendation_model");
 
-var anime_info = {};
-var recommendations = {};
-
 const client = redis.createClient(process.env.REDIS_PORT);
-client.connect();
+
+if(!client.isOpen) {
+    client.connect();
+}
 
 async function get_anime_info(req,res) {
     const {mal_id} = req.params;
-    if(mal_id in anime_info) {
-        res.status(200).json(anime_info[mal_id]);
+
+    const value = await client.get("info: " + mal_id);
+    
+    if(value) {
+        res.status(200).send(value);
+        return;
     }
-    else {
-        const result = await Anime.find({mal_id: mal_id});
-        anime_info[mal_id] = result;
-        res.status(200).json(result);
-    }
+
+    const result = await Anime.find({mal_id: mal_id});
+    client.setEx("info: " + mal_id, 3600, JSON.stringify(...result));
+    res.status(200).json(...result);
+    
 }
 
 async function get_anime_recommendations(req,res) {
     const {mal_id} = req.params;
 
-    if(mal_id in recommendations) {
-        res.status(200).json(recommendations[mal_id]);
+    const value = await client.get("recommendations: " + mal_id);
+    if(value) {
+        res.status(200).send(value);
+        return;
     }
-    else {
-        const result = await Recommendation.find({mal_id: mal_id});
-        var recoms = [];
-        for(let id of result[0].recommendations) {
-            const data = await Anime.find({mal_id: id});
-            recoms.push(...data);
-        }
-        recommendations[mal_id] = recoms;
-        res.status(200).json(recoms);
+
+    const result = await Recommendation.find({mal_id: mal_id});
+    var recoms = [];
+    for(let id of result[0].recommendations) {
+        const data = await Anime.find({mal_id: id});
+        recoms.push(...data);
     }
+
+    client.setEx("recommendations: " + mal_id, 3600, JSON.stringify(recoms));
+    
+    res.status(200).json(recoms);
 }
 
 module.exports = {
